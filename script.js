55f6e375576d41e71250fe2b7e754ed0f724259e
falcaoovo <141254698+falcaoovo@users.noreply.github.com>
2024-10-28 20:18:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function inscrever() {
+    alert("Inscrição iniciada! Preencha o formulário de teste gratuito.");
+}
+
+function verDemo() {
+    alert("Explorando a plataforma!");
+}
+
+function assinar(plano) {
+    alert(`Assinatura no plano ${plano} iniciada!`);
+}
